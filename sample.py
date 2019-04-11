ba95fb67bb296ca1d32372eda9c0e84cdad7fae9
gojiteji <h27i18@ed.cc.suzuka-ct.ac.jp>
2019-04-11 16:13:45 +0900

    funcs

--- sample.py
+++ sample.py
@@ -1,17 +1,34 @@
 import numpy as np
+import tkinter as Tk
 from sklearn.metrics import jaccard_similarity_score
 import matplotlib.pyplot as plt
 
 def jaccard(x,y):
-    #ベクトル表記　Va・Vb / ( |Va|^2 + |Vb|^2 - Va・Vb )
-    result=jaccard_similarity_score(x,y)
-    print(result)
+    #集合表記 |A∧B|/|A∨B|
+    N=np.dot(x,y)
+    D=np.linalg.norm(x)+np.linalg.norm(y)-N
+    print(N/D)
+
+
+def dice():
+    '''
+    2|A∧B|/|A|+|B|
+    分母が共通要素を無視して足しているため分子に二倍
+    '''
+def simpson():
+    '''
+    |A∧B|/小さい方(|A|,|B|)
+    ただし、A∈Bのとき、simは1でもjacは1でないときがある。
+    真部分集合を考慮する必要性がある。
+    '''
+
+
 
 
 
 if __name__ == '__main__':
-    vec_a=['1','0','0']
-    vec_b=['1','0','1']
+    vec_a=np.array([0,1,0])
+    vec_b=np.array([0,1,0])
     jaccard(vec_a,vec_b)
     x = np.arange(-3, 3, 0.1)
     y = np.sin(x)
